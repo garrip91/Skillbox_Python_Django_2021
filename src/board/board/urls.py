9467586bc779da7f0b161910b1b6c938677845b4
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-11-07 12:03:15 +0300

    Update urls.py

--- src/board/board/urls.py
+++ src/board/board/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('advertisement.urls')),
+    path('advertisement/', include('advertisement.urls')),
 ]
