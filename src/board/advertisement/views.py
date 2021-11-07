9ac9178249e7888389784e2e8636b8945cb48181
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-11-07 11:52:32 +0300

    Summary

--- src/board/advertisement/views.py
+++ src/board/advertisement/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 
 def advertisement_list(request, *args, **kwargs):
     
-    return HttpResponse('<ul><li>Мастер на час</li><li>Выведение из запоя</li><li>Услуги экскаватора-погрузчика, гидромолота, ямобура</li></ul>')
+    return render(request, 'advertisement/advertisement_list.html', {})
