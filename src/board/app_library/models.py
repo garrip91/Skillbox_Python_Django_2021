ad624bd569082693ae7b715de2ba83602f99e60b
garrip91 <45920127+garrip91@users.noreply.github.com>
2021-12-11 21:39:24 +0300

    Summary

--- src/board/app_library/models.py
+++ src/board/app_library/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+
+
 # Create your models here:
 class Publisher(models.Model):
 
@@ -8,3 +10,11 @@ class Publisher(models.Model):
     city = models.CharField(max_length=60)
     country = models.CharField(max_length=50)
     is_active = models.BooleanField()
+    
+    
+class Author(models.Model):
+
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=40)
+    email = models.EmailField()
+    biography = models.TextField()
