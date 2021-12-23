bfdb0c60cfe7c8c151dbd64c703ae86fd0c05ea5
garrip91 <garrip91@gmail.com>
2021-12-23 16:46:41 +0300

    Summary

--- src/board/app_users/forms.py
+++ src/board/app_users/forms.py
@@ -4,6 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 
 from django.contrib.auth.models import User
 
+from app_users.models import Profile
+
 
 
 class AuthForm(forms.Form):
@@ -12,17 +14,25 @@ class AuthForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
     
     
-class RegisterForm(UserCreationForm):
+# class RegisterForm(UserCreationForm):
 
-    #first_name = forms.CharField(max_length=30, required=False, help_text='Имя')
-    #last_name = forms.CharField(max_length=30, required=False, help_text='Фамилия')
-    #date_of_birth = forms.DateField(required=True, help_text='Дата рождения')
-    #city = forms.CharField(max_length=36, required=False, help_text='Город')
-    # ****** #
-    print(User._meta.fields)
-    # ****** #
+    # #first_name = forms.CharField(max_length=30, required=False, help_text='Имя')
+    # #last_name = forms.CharField(max_length=30, required=False, help_text='Фамилия')
+    # date_of_birth = forms.DateField(required=True, help_text='Дата рождения')
+    # city = forms.CharField(max_length=36, required=False, help_text='Город')
+    # # ****** #
+    # print(User._meta.fields)
+    # # ****** #
     
+    # class Meta:
+        # model = User
+        # #fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
+        # #fields = ('username', 'first_name', 'last_name', 'date_of_birth ', 'city')
+        # fields = ('username', 'first_name', 'last_name')
+        
+        
+class RegisterForm(forms.ModelForm):
+
     class Meta:
         model = User
-        #fields = ('username', 'first_name', 'last_name', 'password1', 'password2')
-        fields = ('username', 'first_name', 'last_name')
+        fields = ['date_of_birth', 'city']
