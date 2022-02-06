cd11c09a27c7abdcec5c760a3f48ea76f8a94d70
garrip91 <45920127+garrip91@users.noreply.github.com>
2022-02-06 22:05:37 +0300

    Update serializers.py

--- src/board/app_goods/serializers.py
+++ src/board/app_goods/serializers.py
@@ -1,9 +1,18 @@
 from rest_framework import serializers
 
+from .models import DRF_Item
 
 
-class ItemSerializer(serializers.Serializer):
 
-    name = serializers.CharField(max_length=200)
-    description = serializers.CharField(allow_blank=True)
-    weight = serializers.FloatField(min_value=0)
+# class ItemSerializer(serializers.Serializer):
+
+    # name = serializers.CharField(max_length=200)
+    # description = serializers.CharField(allow_blank=True)
+    # weight = serializers.FloatField(min_value=0)
+    
+    
+class DRF_ItemSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = DRF_Item
+        fields = ['id', 'name', 'description', 'weight']
